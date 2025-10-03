a1f006cb614bf09d3ba3472b4e18aaf7bed0d56a
Sinedka <dennnn8888@gmail.com>
2025-10-03 13:04:26 +0300

    removed trunner

--- home-manager/modules/nvim/nvim/lua/config/autocmds.lua
+++ home-manager/modules/nvim/nvim/lua/config/autocmds.lua
@@ -41,3 +41,103 @@ vim.api.nvim_create_autocmd(
     end
   }
 )
+
+-- Функция для сборки C++ файла
+local function build_cpp_file()
+  local current_file = vim.fn.expand("%:p")
+  local file_name = vim.fn.expand("%:t:r")
+  local file_dir = vim.fn.expand("%:p:h")
+  
+  -- Проверяем, что это C++ файл
+  if not vim.endswith(current_file, ".cpp") and not vim.endswith(current_file, ".cc") and not vim.endswith(current_file, ".cxx") then
+    vim.notify("Это не C++ файл!", vim.log.levels.ERROR)
+    return nil
+  end
+  
+  -- Создаем путь к исполняемому файлу
+  local executable_path = file_dir .. "/" .. file_name
+  
+  -- Команда сборки
+  local build_cmd = string.format("cd %s && clang++ -g -O0 -o %s %s", 
+    vim.fn.shellescape(file_dir),
+    vim.fn.shellescape(file_name),
+    vim.fn.shellescape(current_file)
+  )
+  
+  vim.notify("Сборка C++ файла...", vim.log.levels.INFO)
+  
+  -- Выполняем сборку
+  local result = vim.fn.system(build_cmd)
+  local exit_code = vim.v.shell_error
+  
+  if exit_code ~= 0 then
+    vim.notify("Ошибка сборки: " .. result, vim.log.levels.ERROR)
+    return nil
+  end
+  
+  vim.notify("Сборка завершена успешно!", vim.log.levels.INFO)
+  return executable_path
+end
+
+-- Функция для поиска файла с тестом
+local function find_test_file(test_number)
+  local current_file = vim.fn.expand("%:p")
+  local file_name = vim.fn.expand("%:t:r")  -- имя файла без расширения
+  local file_dir = vim.fn.expand("%:p:h")
+  
+  -- Формируем имя файла с тестом
+  local test_file_name = file_name .. "_input" .. test_number .. ".txt"
+  local test_file_path = file_dir .. "/" .. test_file_name
+  
+  -- Проверяем, существует ли файл с тестом
+  if vim.fn.filereadable(test_file_path) == 1 then
+    return test_file_path
+  end
+  
+  -- Если файл не найден, показываем ошибку
+  vim.notify("Файл с тестом не найден: " .. test_file_name, vim.log.levels.ERROR)
+  return nil
+end
+
+-- Функция для запуска DAP с тестом
+local function run_dap_with_test(test_number)
+  local executable_path = build_cpp_file()
+  if not executable_path then
+    return
+  end
+  
+  local test_file_path = find_test_file(test_number)
+  if not test_file_path then
+    return
+  end
+  
+  local dap = require("dap")
+  
+  -- Конфигурация DAP для запуска с тестом
+  local config = {
+    name = "Запуск с тестом " .. test_number,
+    type = "codelldb",
+    request = "launch",
+    program = executable_path,
+    cwd = vim.fn.expand("%:p:h"),
+    stopOnEntry = false,
+    stdio = {test_file_path, null, null}
+  }
+  
+  -- Запускаем DAP
+  dap.run(config)
+end
+
+-- Пользовательская команда для запуска теста
+vim.api.nvim_create_user_command("RunTest", function(opts)
+  local test_number = opts.args
+  if test_number == "" then
+    vim.notify("Укажите номер теста: :RunTest <номер>", vim.log.levels.ERROR)
+    return
+  end
+  
+  run_dap_with_test(test_number)
+end, {
+  nargs = 1,
+  desc = "Собрать C++ файл и запустить DAP с файлом теста (имя_input<номер>.txt)"
+})
--- home-manager/modules/nvim/nvim/lua/plugins/trunner.lua
+++
@@ -1,4 +0,0 @@
-return {
-  'Sinedka/trunner.nvim',
-  cmd = { "RunProgram" }
-}
